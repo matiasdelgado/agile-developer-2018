7a675a7bdda22326360deb55205b3163e84e36d1
Matias Delgado <matiasdel@gmail.com>
2018-11-26 11:57:46 -0300

    Add High Severity gauge

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const issuesDB = require('../models/issue.store');
 
 router.get('/', function(req, res) {
-  res.render('dashboard', {});
+  const issues = issuesDB.getAllOpen();
+  const highSeverity = issues.filter(i => i.severity === 'High');
+
+  let data = {
+    highPercent: 0
+  };
+
+  if (issues.length > 0) {
+    data = {
+      highPercent: highSeverity.length / issues.length
+    };
+  }
+
+  res.render('dashboard', data);
 });
 
 module.exports = router;
--- test/integration/dashboard_spec.js
+++ test/integration/dashboard_spec.js
@@ -1,2 +1,43 @@
 describe('Dashboard', function() {
+  it('show gauges', () => {
+    cy.visit('/dashboard');
+
+    cy
+      .dataTest('high-gauge')
+      .should('exist')
+      .should('contain', '0%')
+      .should('contain', 'High');
+  });
+
+  it('updates the values when issues are added', () => {
+    cy.visit('/dashboard');
+
+    cy.dataTest('add-issue').click();
+    cy.field('title').type('Blue screen in Windows Vista');
+    cy.field('severity').select('High');
+    cy.field('description').type('When I try to play solitaire in Windows, it crashes with a blue screen');
+    cy.get('button').click();
+
+    cy.visit('/dashboard');
+
+    cy
+      .dataTest('high-gauge')
+      .should('exist')
+      .should('contain', '100%')
+      .should('contain', 'High');
+
+    cy.dataTest('add-issue').click();
+    cy.field('title').type('Performance issue when running Minesweeper');
+    cy.field('severity').select('Low');
+    cy.field('description').type('It happens only when running more than one instance');
+    cy.get('button').click();
+
+    cy.visit('/dashboard');
+
+    cy
+      .dataTest('high-gauge')
+      .should('exist')
+      .should('contain', '50%')
+      .should('contain', 'High');
+  });
 });
